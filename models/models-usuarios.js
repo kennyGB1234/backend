be6d559b18aeeeb790714d7440a95b37157efd05
Kenny Garcia Benitez <kennylpgc@gmail.com>
2023-05-05 10:01:50 +0100

    backend

---
+++ models/models-usuarios.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const usuarioSchema = new mongoose.Schema({
+  nombre: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  coches: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Coche",
+    },
+  ],
+  publicados: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Coche",
+    },
+  ],
+});
+
+module.exports = mongoose.model("Usuario", usuarioSchema);
